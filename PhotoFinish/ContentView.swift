028df2d1dbda11ffe041d89495d650559e1addf7
AndreasPapaz <andreaspapaz@gmail.com>
2020-04-13 22:47:05 -0500

    Initial Commit

---
+++ PhotoFinish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoFinish
+//
+//  Created by Andreas Papazafeiropoulos on 4/13/20.
+//  Copyright © 2020 1401Apps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
